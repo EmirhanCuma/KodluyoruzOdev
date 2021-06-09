980797a766082b79595a1ca4152506d09bff6d9c
Emirhan Cuma Arslan <emirhancuma2004@hotmail.com>
2021-06-09 08:45:02 +0300

    Kodluyoruz Console Programlama Ödevi

---
+++ Kodluyoruz_ConsoleProgramlama/Kodluyoruz_ConsoleProgramlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kodluyoruz_ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz: ");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("********************************");
+            Console.WriteLine("Merhaba");
+            Console.WriteLine("Adın: " + isim);
+            Console.WriteLine("Soyadın: " + soyad);
+        }
+    }
+}
